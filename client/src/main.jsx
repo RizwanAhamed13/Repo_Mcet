9fceaca974cfd7b701c788102807388b7ae875c4
RizwanAhamed13 <rizwanahamed2726@gmail.com>
2025-07-14 00:51:58 +0530

    Migrate from Create React App to Vite for faster builds and better performance

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,12 +6,12 @@ import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import App from './App';
+import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext';
 import { PrintJobProvider } from './context/PrintJobContext';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = ReactDOM.createRoot(document.getElementById('root')).render(
+// 
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
@@ -33,4 +33,4 @@ root.render(
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
-); 
+,) 
